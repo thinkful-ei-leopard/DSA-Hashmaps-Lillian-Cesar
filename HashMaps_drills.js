528a0eefc4c39bc389be5b98563d06cdda846f47
Poden <56049210+Poden@users.noreply.github.com>
2020-04-06 13:48:50 -0500

    update

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -23,3 +23,18 @@ function main() {
 }
 
 console.log(main());
+
+
+
+//Drill 2
+// Making sure that we understand how variables can be overriden.
+
+
+
+
+
+// Drill 3 
+
+HashMapArray = [22,10,88,X,4,28,17,15,,59,31,X];
+
+SeparateChainingArray =[X,28 -> 19 -> 10, 20, 12,5,X,15 -> 33, X,17];
